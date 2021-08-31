e21cad3398b7b060575e87c8868e9e4b2d96762d
Jeffry Gonzalez <jeff@hypertheory.com>
2021-08-30 21:04:24 -0400

    updates

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Hypertheory</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Hypertheory Training Resources
       </span>
     </>
   ),
